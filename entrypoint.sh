7fb784e0f8b05ce344b04611c69bddb109b1d0ba
runnymamaly <alternative2236@atomicmail.io>
2026-06-07 23:08:35 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
-export PATH="/opt:$PATH"
+echo "export PATH="/opt:/tmp:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"" >> /etc/profile
 if ! command -v xray >/dev/null; then
-  7z x -y -bsp0 -bso0 "xray.7z" -o"/opt"
-  chmod +x "/opt/xray"
+  7z x -y -bsp0 -bso0 "xray.7z" -o"/tmp"
+  chmod +x "/tmp/xray"
   chmod +x "/opt/command.sh"
 fi
 exec "$@"
