e9840a565499c10c40838312a386e07ac0b690df
runnymamaly <alternative2236@atomicmail.io>
2026-06-12 18:01:19 +0330

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 if ! command -v xcore >/dev/null; then
   7z x -y -bsp0 -bso0 "xcore.7z" -o"/usr/local/bin/"
-  rm -rf "/opt/xcore" "/opt/core-linux-64.zip"
+  rm -rf "/opt/xcore" "/opt/xcore.7z" "/opt/core-linux-64.zip"
   chmod +x "/usr/local/bin/xcore"
   chmod +x "/opt/command.sh"
   chmod +x "/opt/configs.sh"
